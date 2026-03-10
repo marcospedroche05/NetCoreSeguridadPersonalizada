abe8cce1919aed2873ce6b66ba52700e398420a0
marcospedroche05 <marcospedroche62@gmail.com>
2026-03-10 11:26:46 +0100

    Add project files.

---
+++ NetCoreSeguridadPersonalizada/Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSeguridadPersonalizada.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
